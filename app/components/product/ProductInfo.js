7305d8e2c31418aa83c7c6e883e7926db0f0b36b
siddighope <noory4433@gmail.com>
2021-12-28 21:15:11 +0200

    first commit

---
+++ app/components/product/ProductInfo.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+export default class ProductInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> ProductInfo </Text>
+      </View>
+    );
+  }
+}
